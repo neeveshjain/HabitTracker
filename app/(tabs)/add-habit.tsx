47de0e3e0bda44e5c0cfd78d8a9d4a3466853f54
Neevesh Jain <neeveshjain27@gmail.com>
2026-01-03 11:51:16 +0530

    #11 Building the Add Habit Screen UI, also added TouchWithoutFeedback to handle keyboard issue in iOS.

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,9 +1,49 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Keyboard, StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import { Button, SegmentedButtons, TextInput } from "react-native-paper";
 
-export default function addhabitScreen () {
+const frequencies = ["daily", "weekly", "montly"];
+type frequency = (typeof frequencies)[number];
+export default function addhabitScreen() {
+
+  const [title,setTitle] = useState<string>("");
+  const [description,setDesciption] = useState<string>("");
+  const [frequency,setFrequency] = useState<frequency>("daily");
+
   return (
-    <View>
-      <Text>Hello this is login screen</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <View style={styles.container}>
+      <TextInput label="Title" mode="outlined" style={styles.input} onChangeText={setTitle}/>
+      <TextInput label="Description" mode="outlined" style={styles.input} onChangeText={setDesciption}/>
+
+      <View style={styles.frequencyContainer}>
+        <SegmentedButtons
+          value={frequency}
+          onValueChange={(value)=> setFrequency(value as frequency)}
+          buttons={frequencies.map((freq) => ({
+            value: freq,
+            label: freq.charAt(0).toUpperCase() + freq.slice(1),
+          }))}
+        />
+      </View>
+      <Button mode="contained" disabled={!title || !description}>Add Habit</Button>
     </View>
-  )
-}  
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    padding:16,
+    backgroundColor:"#f5f5f5",
+    justifyContent:"center"
+
+  },
+  input:{
+    marginBottom:16
+  },
+  frequencyContainer:{
+    marginBottom:24
+  }
+});
